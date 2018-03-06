4590185108d2a10801fd45c8367c39f04749d356
Nandeshwa Sah <nandeshwar.sah@dish.com>
2018-03-06 12:23:26 -0700

    cleaned the code for displaying linked list items

--- linked_list_example.clj
+++ linked_list_example.clj
@@ -18,62 +18,38 @@
   (setId [this x] (set! id x) this)
   (setNext [this x] (set! next x) this))
 
-
-
-(def start (ref nil))
-(def begin (ref start)) ; getting used in display
-(def current (ref nil))
+(def start nil)
+(def begin nil)
+(def current nil)
 (def is-first-item true)
-
-
-(defn update-start [node]
-  (dosync
-   (ref-set start node)))
-
-(defn update-current [node]
-  (dosync
-   (ref-set current node)))
-
-(defn update-begin [node]
-  (dosync
-   (ref-set begin node)))
 
 (defn linked-list []
   (println "Linked list demonstration")
   (add 10)
-  (println start)
-  (println (.getId (deref current)))
   (add 20)
-  (println (.getId (deref current)))
   (add 30)
 
-  (println (.getId (deref begin)))
-  (println (.getId (.getNext (deref start))))
-  (println "____________")
-  (display)
-;  (println (.getId (.getNext begin)))
-  )
+  (display))
 
 
 (defn- add [item]
   (if (true? is-first-item)
     (do
 
-      (update-start (Node. item nil ))
-      ;(println (.getId (deref start)))
-      (update-current (deref start))
-      (update-begin @current)
+      (def start (Node. item nil ))
+      (def current start)
+      (def begin start)
       (def is-first-item false))
     (do
-      (.setNext (deref current) (Node. item nil ))
-      (update-current (.getNext (deref current))))))
+      (.setNext current (Node. item nil ))
+      (def current (.getNext current)))))
 
 (defn- display []
-
-  (if (= (.getNext (deref begin)) nil)
-    nil
+  (if (= (.getNext begin) nil)
     (do
-      (println (.getId (deref begin)))
-      (update-begin (.getNext (deref begin)))
-      (recur)))
-  )
+      (println (.getId begin))
+      nil)
+    (do
+      (println (.getId begin))
+      (def begin (.getNext begin))
+      (recur))))

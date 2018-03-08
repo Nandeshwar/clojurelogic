2b3486516294c37e7a29a1326b6138ceeba39896
Nandeshwa Sah <nandeshwar.sah@dish.com>
2018-03-08 10:09:20 -0700

    Added function to insert item in between

--- linked_list_example.clj
+++ linked_list_example.clj
@@ -2,19 +2,30 @@
 
 (declare add)
 (declare display)
+(declare insert-item)
 
 (defprotocol INode
   (getId [this])
   (getNext [this])
-  (setNext [this y]))
+  (setNext [this y])
+  (display1 [this x]))
 
 (deftype Node [id ^:volatile-mutable next]
   INode
   (getId [this] id)
   (getNext [this] next)
-  (setNext [this nw-node] (set! next nw-node) next))
+  (setNext [this nw-node] (set! next nw-node) next)
 
-
+  ; This method is equivalent to display method given below
+  ; I was just testing to have this interface
+  (display1 [this node]
+            (if (nil? (.getNext node))
+              (do
+                (println (.getId node))
+                nil)
+              (do
+                (println (.getId node))
+                (recur (.getNext node))))))
 
 (def start nil)
 (def current nil)
@@ -26,7 +37,14 @@
   (add 20)
   (add 30)
 
-  (display start))
+  (insert-item 20 200)
+  (display start)
+  (println "------------Display again------")
+  ; display1 is method
+  ; start 2nd parameter:  is object on which display1 is getting called
+  ; 3rd parameter : passint object
+  (.display1 start start))
+
 
 (defn- add [item]
   (if (true? is-first-item)
@@ -46,3 +64,20 @@
     (do
       (println (.getId begin))
       (recur (.getNext begin)))))
+
+(defn- insert-item [after item]
+  (loop [current start]
+    (if (nil? (.getNext current))
+      (do
+        (println (.getId current))
+        false)
+      (do
+        (if (= (.getId current) after)
+          (do
+            (def new-node (Node. item (.getNext current)))
+            (def tmp (getNext current))
+            (.setNext current new-node)
+            (.setNext new-node tmp)
+            true)
+          (recur (.getNext current)))))))
+
